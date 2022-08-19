aa6160e017f914fb2d1a536db7055984cba092f5
reneuam <renet@uassistme.com>
2022-08-19 10:44:46 -0600

    API URLs

---
+++ common/constants/api.ts
@@ -0,0 +1,5 @@
+export const Api = {
+  ATTENDANCE: `${process.env.NEXT_PUBLIC_UAM_API_URL}/v1/attendance`,
+  TEAM: `${process.env.NEXT_PUBLIC_UAM_API_URL}/v1/team`,
+  TIMEOFF: `${process.env.NEXT_PUBLIC_UAM_API_URL}/v1/timeoff`
+}
