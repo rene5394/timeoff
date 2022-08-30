a9e989f70023f73fbe8fa6d6dd229614d962048d
reneuam <renet@uassistme.com>
2022-08-29 21:31:36 -0600

    Add instance template for axios

--- lib/api/auth/auth.ts
+++ lib/api/auth/auth.ts
@@ -1,9 +1,14 @@
 import axios from 'axios';
 import { Api } from '../../../common/constants/api';
 
+const instance = axios.create({
+  baseURL: `${Api.AUTH}`,
+  withCredentials: true
+})
+
 export const login = async(form: any) => {
-  const url =  `${Api.AUTH}/login`;
-  const result = await axios.post<any>(url, {
+  const url =  '/login';
+  const result = await instance.post<any>(url, {
     email: form.target.elements.email.value,
     password: form.target.elements.password.value
   }, {withCredentials: true});
