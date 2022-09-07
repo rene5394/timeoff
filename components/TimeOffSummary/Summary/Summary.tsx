c5e4ac76d174f87f5a0206123a8e624655d42306
reneuam <renet@uassistme.com>
2022-09-07 13:15:23 -0600

    Load balance and balance pending on time off summary

--- components/TimeOffSummary/Summary/Summary.tsx
+++ components/TimeOffSummary/Summary/Summary.tsx
@@ -1,11 +1,45 @@
 import * as React from 'react';
-import { getCurrentDate } from '../../../common/utils/timeValidation';
+import { daysBetweenDates, daysBetweenDatesNoWeekends, getCurrentDate } from '../../../common/utils/timeValidation';
 import { IBalance } from '../../../lib/domain/timeoff/IBalance';
-import { Requests } from '../Requests';
+import { IPendingBalance } from '../../../common/interfaces/IPendingBalance';
+import { findOneByUserJWT } from '../../../lib/api/timeoff/balance';
+import { findAllRequestByUserJWTAndStatus } from '../../../lib/api/timeoff/request';
+import { RequestType } from '../../../common/enums/request-type.enum';
 import Styles from './Summary.module.css';
 
 export const Summary = () =>{
   const [balance, setBalance] = React.useState<IBalance>();
+  const [pendingBalance, setPendingBalance] = React.useState<IPendingBalance>();
+
+  React.useEffect(() => {
+    const fillBalanceData = async() => {
+      const result = await findOneByUserJWT();
+      setBalance(result);
+    };
+    fillBalanceData();
+  }, [])
+
+  React.useEffect(() => {
+    const fillPendingData = async() => {
+      const requests = await findAllRequestByUserJWTAndStatus('pending');
+      let compDays = 0;
+      let vacationDays = 0;
+
+      requests.map((request) => {
+        if (request.typeId === RequestType.compDay) {
+          const dates = daysBetweenDatesNoWeekends(request.startDate, request.endDate);
+          console.log(dates, dates.length);
+          compDays += dates.length;
+        } if (request.typeId === RequestType.vacation) {
+          const dates = daysBetweenDates(request.startDate, request.endDate);
+          vacationDays += dates.length;
+        }
+      });
+
+      setPendingBalance({compDays, vacationDays});
+    };
+    fillPendingData();
+  }, [])
   
   return(
     <div className={"col " + Styles.SummaryDiv}>
@@ -32,14 +66,14 @@ export const Summary = () =>{
               <tr>
                 <th>Comp Day</th>
                 <td>15 d</td>
-                <td>0 d</td>
-                <td>1 d</td>
+                <td>{String(balance?.compDays)} d</td>
+                <td>{String(pendingBalance?.compDays)} d</td>
               </tr>
               <tr>
                 <th>Vacation</th>
                 <td>15 d</td>
-                <td>7 d</td>
-                <td>0 d</td>
+                <td>{String(balance?.vacationDays)} d</td>
+                <td>{String(pendingBalance?.vacationDays)} d</td>
               </tr>
             </tbody>
           </table>
