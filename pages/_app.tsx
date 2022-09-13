f150c61a04e7fe4dd07db9d4deb4e4012059e4fa
reneuam <renet@uassistme.com>
2022-09-12 20:39:30 -0600

    Fix bootstrap and bootstrap icons import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';                                                                                                                                                                                                                                                                                                                                                                       
 import '../styles/global.css';
 import type { AppProps } from 'next/app';
-import { useEffect } from "react";
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false;
@@ -11,10 +11,6 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 library.add(fas)
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(()=>{
-    import('bootstrap/dist/js/bootstrap.js');
-    import('bootstrap-icons/font/bootstrap-icons.css');
-  },[])
   return <Component {...pageProps} />
 }
 
