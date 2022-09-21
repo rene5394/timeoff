b351925ed2d4dbec2ce65ac2c6d69693ad1ce260
reneuam <renet@uassistme.com>
2022-09-21 17:00:10 -0600

    Load user's team

--- components/StaffDirectory/StaffTable/StaffTable.tsx
+++ components/StaffDirectory/StaffTable/StaffTable.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { findAllUsersEmployees } from '../../../lib/api/team/user';
 import { findBalances } from '../../../lib/api/timeoff/balance';
+import { findAllTeams } from '../../../lib/api/team/team';
+import { findEmployees } from '../../../lib/api/team/employee';
+import { findMembers } from '../../../lib/api/team/member';
 import { IUser } from '../../../lib/domain/team/IUser';
 import Moment from 'moment';
-import { findAllTeams } from '../../../lib/api/team/team';
 
 interface IUserData extends IUser {
   compDays?: number;
@@ -16,20 +18,42 @@ export const StaffTable = () => {
 
   React.useEffect(() => {
     const fillUserData = async() => {
-      const teams = await findAllTeams();
-      const users = await findAllUsersEmployees();
-      const balances = await fillBalances(users);
+      let userIds: any = [];
+      let employeesIds: any = [];
 
-      const objs: any = [];
+      const teams = await findAllTeams();
+      const users = await findAllUsersEmployees();      
+
+      users.map((user) => userIds.push(user.id));
+      const balances = await findBalances(userIds);
+      const employees = await findEmployees(userIds);
+      
+      employees.map((employee) => employeesIds.push(employee.id));
+      const members = await findMembers(employeesIds);
+
+      const objs: IUserData[] = [];
 
       users.map(user => {
         const obj: IUserData = { ...user };
+
         balances.map(balance => {
           if (user.id === balance.userId) {
             obj.compDays = balance.compDays;
             obj.vacationDays = balance.vacationDays;
           }
         });
+
+        employees.map(employee => {
+          if (user.id === employee.user_id) {
+            members.map((member) => {
+              if (member.employee_id === employee.id) {                
+                const team = teams.find(team => team.id === member.team_id);
+                obj.teamName = (team) ? team.name : '';
+              }
+            });
+          }
+        });
+
         objs.push(obj);
       });
       
@@ -38,19 +62,9 @@ export const StaffTable = () => {
     fillUserData();
   }, [])
 
-  const fillBalances = async(users: IUser[] | undefined) => {
-    let userIds: any = [];
-
-    users?.map((user) => {
-      userIds.push(user.id);
-    });
-    
-    return await findBalances(userIds);
-  };
-
   return(
     <div className="row px-5 pt-4">
-      <table className="table align-middle mb-0 bg-white">
+      <table className="table align-middle mb-5 bg-white">
         <thead className="bg-light">
           <tr>
             <th>Employee</th>
@@ -63,7 +77,7 @@ export const StaffTable = () => {
         </thead>
         <tbody>
           { usersData?.map((userData) => 
-              <tr id={userData.id.toString()} key={userData.id.toString()}>
+              <tr key={userData.id.toString()}>
                 <td>
                   <div className="d-flex align-items-center">
                     <img
@@ -77,10 +91,7 @@ export const StaffTable = () => {
                     </div>
                   </div>
                 </td>
-                <td>
-                  <p className="fw-normal mb-1">Software engineer</p>
-                  <p className="text-muted mb-0">IT department</p>
-                </td>
+                <td>{userData.teamName}</td>
                 <td>{userData.compDays?.toString()}</td>
                 <td>{userData.vacationDays?.toString()}</td>
                 <td>{Moment(userData.hiredate).format('MM-DD-YYYY')}</td>
