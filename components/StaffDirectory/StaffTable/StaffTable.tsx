9844f75afdc6299a00569f94846f9c0843803636
reneuam <renet@uassistme.com>
2022-09-23 12:25:54 -0600

    Add pagination to staffTable

--- components/StaffDirectory/StaffTable/StaffTable.tsx
+++ components/StaffDirectory/StaffTable/StaffTable.tsx
@@ -8,6 +8,7 @@ import { IBalance } from '../../../lib/domain/timeoff/IBalance';
 import { IUser } from '../../../lib/domain/team/IUser';
 import Moment from 'moment';
 import { IEmployee } from '../../../lib/domain/team/IEmployee';
+import AdvancedPagination from '../../Commons/AdvancedPagination';
 
 interface IUserData extends IUser {
   compDays?: number;
@@ -17,56 +18,65 @@ interface IUserData extends IUser {
 
 export const StaffTable = () => {
   const [usersData, setUsersData] = React.useState<any[]>();
+  const [numberOfPages, setNumberOfPages] = React.useState<number>(1);
+  const [activePage, setActivePage] = React.useState<number>(1);
 
   React.useEffect(() => {
-    const fillUserData = async() => {
-      let userIds: any = [];
-      let employeesIds: any = [];
-
-      const teams = await findAllTeams();
-      const users = await findAllUsersEmployees();      
-
-      users.map((user) => userIds.push(user.id));
-      const balances = await findBalances(userIds);
-      const employees = await findEmployees(userIds);
-      
-      employees.map((employee) => employeesIds.push(employee.id));
-      const members = await findMembers(employeesIds);
-
-      const objs: IUserData[] = [];
-
-      users.map((user: IUser) => {
-        const obj: IUserData = { ...user };
-
-        balances.map((balance: IBalance) => {
-          if (user.id === balance.userId) {
-            obj.compDays = balance.compDays;
-            obj.vacationDays = balance.vacationDays;
-          }
-        });
-
-        employees.map((employee: IEmployee) => {
-          if (user.id === employee.user_id) {
-            members.map((member) => {
-              if (member.employee_id === employee.id) {                
-                const team = teams.find(team => team.id === member.team_id);
-                obj.teamName = (team) ? team.name : '';
-              }
-            });
-          }
-        });
-
-        objs.push(obj);
-      });
-      
-      setUsersData(objs);
-    };
-    fillUserData();
+    fillUserData(1);
   }, [])
+
+  const fillUserData = async(page: number) => {
+    let userIds: any = [];
+    let employeesIds: any = [];
+
+    const teams = await findAllTeams();
+    const users = await findAllUsersEmployees(page);
+
+    users.map((user) => userIds.push(user.id));
+    const balances = await findBalances(userIds);
+    const employees = await findEmployees(userIds);
+    
+    employees.map((employee) => employeesIds.push(employee.id));
+    const members = await findMembers(employeesIds);
+
+    const objs: IUserData[] = [];
+
+    users.map((user: IUser) => {
+      const obj: IUserData = { ...user };
+
+      balances.map((balance: IBalance) => {
+        if (user.id === balance.userId) {
+          obj.compDays = balance.compDays;
+          obj.vacationDays = balance.vacationDays;
+        }
+      });
+
+      employees.map((employee: IEmployee) => {
+        if (user.id === employee.user_id) {
+          members.map((member) => {
+            if (member.employee_id === employee.id) {                
+              const team = teams.find(team => team.id === member.team_id);
+              obj.teamName = (team) ? team.name : '';
+            }
+          });
+        }
+      });
+
+      objs.push(obj);
+    });
+    
+    setUsersData(objs);
+    setNumberOfPages(2);
+  };
+
+  const changePage = (page: number) => {
+    setActivePage(page);
+    fillUserData(page);
+  }
 
   return(
     <div className="row px-5 pt-4">
-      <table className="table align-middle mb-5 bg-white">
+      <table className="table align-middle mb-4 bg-white">
         <thead className="bg-light">
           <tr>
             <th>Employee</th>
@@ -106,6 +116,7 @@ export const StaffTable = () => {
             )}
         </tbody>
       </table>
+      <AdvancedPagination activePage={activePage} numberOfPages={numberOfPages} changePage={changePage} />
     </div>
   );
 }
