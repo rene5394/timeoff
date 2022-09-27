44e0ad7ec9099a9dafb30bb6d835f477ca6887f8
reneuam <renet@uassistme.com>
2022-09-27 12:03:52 -0600

    Set advanced pagination limit of 10 pages and set first and last page

--- components/Commons/AdvancedPagination.tsx
+++ components/Commons/AdvancedPagination.tsx
@@ -31,18 +31,37 @@ export const AdvancedPagination: React.FC<Pagination> = ({ activePage, numberOfP
 
   let paginations = [];
 
-  for (let i = 1; i < numberOfPages; i++) {
+  if (numberOfPages > 2 && numberOfPages <= 10) {
+    
+    for (let i = 1; i < numberOfPages; i++) {
+      paginations.push(<Pagination.Item onClick={()=> changePage(i + 1)} key={i + 1} active={activePage === i + 1}>{i + 1}</Pagination.Item>)
+    }
+
+    return (
+      <Pagination className="mb-4">
+        <Pagination.First onClick={()=> changePage(1)} disabled={activePage === 1} />
+        <Pagination.Prev onClick={()=> changePage(activePage - 1)} disabled={activePage === 1} />
+        <Pagination.Item onClick={()=> changePage(1)} key={1} active={activePage === 1}>{1}</Pagination.Item>
+        {paginations}
+        <Pagination.Next onClick={()=> changePage(activePage + 1)} disabled={activePage === numberOfPages} />
+        <Pagination.Last onClick={()=> changePage(numberOfPages)} disabled={activePage === numberOfPages} />
+      </Pagination>
+    );
+  }
+  
+  for (let i = 1; i < 9; i++) {
     paginations.push(<Pagination.Item onClick={()=> changePage(i + 1)} key={i + 1} active={activePage === i + 1}>{i + 1}</Pagination.Item>)
   }
 
   return (
     <Pagination className="mb-4">
-      <Pagination.First />
-      <Pagination.Prev onClick={()=> changePage(activePage - 1)} />
+      <Pagination.First onClick={()=> changePage(1)} disabled={activePage === 1} />
+      <Pagination.Prev onClick={()=> changePage(activePage - 1)} disabled={activePage === 1} />
       <Pagination.Item onClick={()=> changePage(1)} key={1} active={activePage === 1}>{1}</Pagination.Item>
       {paginations}
-      <Pagination.Next onClick={()=> changePage(activePage + 1)} />
-      <Pagination.Last />
+      <Pagination.Item onClick={()=> changePage(10)} key={10} active={activePage === 10}>{10}</Pagination.Item>
+      <Pagination.Next onClick={()=> changePage(activePage + 1)} disabled={activePage === 10} />
+      <Pagination.Last onClick={()=> changePage(10)} disabled={activePage === 10} />
     </Pagination>
   );
 }
