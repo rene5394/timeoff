c642716046986a35fb76d5536606622f2bc0a9dc
Joel Castillo <joelc@uassistme.com>
2022-09-28 16:20:45 -0600

    Add Summary filter by Year

--- components/TimeOffSummary/Summary/Summary.tsx
+++ components/TimeOffSummary/Summary/Summary.tsx
@@ -4,21 +4,9 @@ import Styles from './Summary.module.css';
 
 export const Summary = () =>{
   return(
-    <div className={"col " + Styles.SummaryDiv}>
-      <div className="row">
-        <div className="col-5">
-          <h3>Time-Off Summary</h3>
-
-          <label htmlFor="Start">START DATE</label>
-          <input type='Date' id='Start' defaultValue={ String(new Date()) } className='form-control' />
-          
-        </div>
-        <div className={"col-7 " + Styles.Allocation}>
-          <h4>Time-Off Type Allocation</h4>
-          <MyAllocation />
-
-        </div>
-      </div>
+    <div className={"col-7 " + Styles.Allocation}>
+      <h4>Time-Off Type Allocation</h4>
+      <MyAllocation />
     </div>
   );
 }
