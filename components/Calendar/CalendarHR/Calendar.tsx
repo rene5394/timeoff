a6b2957f1361dffcc7287542142e8d37269eb9ef
Joel Castillo <joelc@uassistme.com>
2022-10-03 15:02:17 -0600

    Calendar admin v1

--- components/Calendar/CalendarHR/Calendar.tsx
+++ components/Calendar/CalendarHR/Calendar.tsx
@@ -9,8 +9,10 @@ import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import 'moment-timezone';
 import Styles from './Calendar.module.css';
-import { IEvents } from '../../../lib/domain/timeoff/IEvents';
-import { findNumberOfRequestsByYearMonth } from '../../../lib/api/timeoff/request';
+import { IEventsDetails } from '../../../lib/domain/timeoff/IEvents';
+import { findRequestsByYearMonth } from '../../../lib/api/timeoff/request';
+import { IUser } from '../../../lib/domain/team/IUser';
+import { findUsers } from '../../../lib/api/team/user';
 
 moment.tz.setDefault('America/El_Salvador');
 const localizer = momentLocalizer(moment);
@@ -24,33 +26,66 @@ interface ICalendarEvent {
 } 
 
 export const Calendar = () => {
-  const [events, setEvents] = React.useState<IEvents[]>();
+  const [events, setEvents] = React.useState<IEventsDetails[]>();
   const [calendarEvents,setCalendarEvents]= React.useState<ICalendarEvent[]>();
   const [dates,setDates] = React.useState<any>();
+  const [users, setUsers] = React.useState<IUser[]>();
 
   const fillEvents = async(compDates:Date) => {
     let year, month;
     month = compDates.getMonth()+1;
     year = compDates.getFullYear();
     let result, resultRequest;
-    result = await findNumberOfRequestsByYearMonth(year,month);
-    resultRequest = result.filter(ev => ev.number != 0);
+    result = await findRequestsByYearMonth(year,month);
+    resultRequest = result.filter(ev => ev.requests.length > 0);
     setEvents(resultRequest);
+    callUsers();
     fillCalendarEvents();
+  }
+
+  const callUsers = async() => {
+    let usersId: any[] = [];
+    if (events) {
+      events.map(event => {
+        event.requests.map(req => {
+          usersId.push(req.userId);
+        });
+        
+      });
+    }
+    console.log('Id de usuarios desde events',usersId);
+    let result = await findUsers(usersId);
+    
+    setUsers(result);
+    console.log('usuarios devueltos',result);
+  }
+
+  const findName = (id:number) => {
+    if (users) {
+      console.log('findName',users);
+      console.log('id',id);
+      let user = users.find(us => us.id = id);
+      console.log('usuario',user);
+      return String(user?.firstname+' '+user?.lastname);
+    }
+    return 'Not found';
   }
 
   const fillCalendarEvents = () => {
     let calendarEvent;
     if (events) {
-      const newCalendarEvent = events.map((event,i) => {
-        calendarEvent = {
-          id: i,
-          title: String(event.number),
-          start: event.day,
-          end: event.day,
-          allDay: true
-        }
-        return calendarEvent;
+      const newCalendarEvent: ICalendarEvent[] = [];
+      events.map((event) => {
+        event.requests.map(req => {
+          calendarEvent = {
+            id: req.id,
+            title: findName(req.userId),
+            start: req.day,
+            end: req.day,
+            allDay: true
+          }
+          newCalendarEvent.push(calendarEvent);
+        });
       } 
       );
       setCalendarEvents(newCalendarEvent);
