4f602079b61a19b6f11b0e0442545eeee59776a0
reneuam <renet@uassistme.com>
2022-10-06 10:05:24 -0600

    Add endpoints for request statuses

---
+++ lib/api/timeoff/requestStatus.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { Api } from "../../../common/constants/api";
+import { IRequestStatus } from '../../domain/timeoff/IRequestStatus';
+
+const instance = axios.create({
+  baseURL: `${Api.TIMEOFF}`,
+  withCredentials: true
+});
+
+instance.interceptors.response.use((response) => {
+  return response;
+}, (error) => {
+  if (error.response.status === 401) {
+    window.location.href = '/login';
+  }
+
+  return Promise.reject(error);
+});
+
+export const findAllRequestStatuses = async() => {
+  const url = '/statuses';
+  const result = await instance.get<IRequestStatus[]>(url);
+
+  return result.data;
+};
+
+export const findOneRequestStatus = async(id: number) => {
+  const url = `/statuses/${id}`;
+  const result = await instance.get<IRequestStatus>(url);
+
+  return result.data;
+};
