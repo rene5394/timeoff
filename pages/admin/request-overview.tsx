d87e133f358e15a1f2f637ed34f821b6a01cda0f
reneuam <renet@uassistme.com>
2022-10-06 10:04:00 -0600

    Add popup useStates for request overview page

--- pages/admin/request-overview.tsx
+++ pages/admin/request-overview.tsx
@@ -1,12 +1,44 @@
+import React from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { PersonalDetails } from '../../components/Profile/PersonalDetails';
 import { NavHeader } from '../../components/Layout/NavHeader';
 import { SideBarAdmin } from '../../components/Layout/Sidebars/SidebarAdmin';
-import { Banner } from '../../components/Profile/Banner/Banner';
-import { MyBalance } from '../../components/Profile/MyBalance';
+import { RequestTable } from '../../components/RequestOverview/RequestTable';
+import { SuccessModal, SuccessModalTextProps } from '../../components/Modals/SucessModal';
+import { ErrorModal, ErrorModalTextProps } from '../../components/Modals/ErrorModal';
 
 const RequestOverview: NextPage = () => {
+  const [successModalVisibility, setSuccessModalVisibility] = React.useState<boolean>(false);
+  const [errorModalVisibility, setErrorModalVisibility] = React.useState<boolean>(false);
+  const [modalSuccessText, setModalSuccessText] = React.useState<SuccessModalTextProps>();
+  const [modalErrorText, setModalErrorText] = React.useState<ErrorModalTextProps>();
+
+  const openSuccessModal = (textProps: SuccessModalTextProps) => {
+    setModalSuccessText({
+      title: textProps.title,
+      body: textProps.body
+    });
+    
+    setSuccessModalVisibility(true);
+  }
+
+  const openErrorModal = (textProps: ErrorModalTextProps) => {
+    setModalErrorText({
+      title: textProps.title,
+      body: textProps.body
+    });
+
+    setErrorModalVisibility(true);
+  }
+
+  const closeSuccessModal = () => {
+    setSuccessModalVisibility(false);
+  }
+
+  const closeErrorModal = () => {
+    setErrorModalVisibility(false);
+  }
+
   return (
     <div className='container'>
       <Head>
@@ -15,15 +47,16 @@ const RequestOverview: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavHeader />
-      <div className='body row mx-0'>
+      <div className="body row mx-0">
         <SideBarAdmin />
-        <div className='content col-8 pb-5'>
-          <Banner />
-          <PersonalDetails />
-          <MyBalance />
+        <div className="col-8">
+          <div className="content">
+            <RequestTable openSuccessModal={openSuccessModal} openErrorModal={openErrorModal} />
+          </div>
         </div>
+        <SuccessModal text={modalSuccessText} visibility={successModalVisibility} closeModal={closeSuccessModal} />
+        <ErrorModal text={modalErrorText} visibility={errorModalVisibility} closeModal={closeErrorModal} />
       </div>
-      
     </div>
   )
 }
