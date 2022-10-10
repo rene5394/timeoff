00be5be14b6c7520cea553842795f545c76fcb6d
reneuam <renet@uassistme.com>
2022-10-10 07:04:13 -0600

    Add functions and components for request overview for admin and coach

--- pages/admin/request-overview.tsx
+++ pages/admin/request-overview.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import { NavHeader } from '../../components/Layout/NavHeader';
 import { SideBarAdmin } from '../../components/Layout/Sidebars/SidebarAdmin';
-import { RequestTable } from '../../components/RequestOverview/RequestTable';
+import { RequestTable } from '../../components/RequestOverview/Admin/RequestTable';
 import { SuccessModal, SuccessModalTextProps } from '../../components/Modals/SucessModal';
 import { ErrorModal, ErrorModalTextProps } from '../../components/Modals/ErrorModal';
 
