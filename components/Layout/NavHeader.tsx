60e818252c12ea0c95a652fb9457a8d89b72e906
reneuam <renet@uassistme.com>
2022-10-27 07:10:17 -0600

    Hide notifications and news icons on navigation

--- components/Layout/NavHeader.tsx
+++ components/Layout/NavHeader.tsx
@@ -29,17 +29,17 @@ export const NavHeader = () => {
     <nav className="navbar navbar-expand-lg">
       <div className="container">
         <a href="#" className="navbar-brand">
-          <Image src="/assets/img/uam.png" alt="Vercel Logo" width={60} height={30} className="d-inline-block align-text-top"/>
+          <Image src="/assets/img/uam.png" alt="UAM Logo" width={60} height={30} className="d-inline-block align-text-top"/>
         </a>
         <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarCollapse">
           <div className="navbar-nav ms-auto">
-            <a className="nav-link active" aria-current="page" href="#">
+            <a className="nav-link active d-none" aria-current="page" href="#">
               <Image src='/assets/icons/bell.png' alt="notifications" width={15} height={15} />
             </a>
-            <a className="nav-link" href="#">
+            <a className="nav-link d-none" href="#">
               <Image src='/assets/icons/papernews.png' alt="news" width={15} height={15} />
             </a>
             <div className="nav-item dropdown row">
