eec5a597f195158b4c243014d4de94fd5c3226ea
reneuam <renet@uassistme.com>
2022-10-27 10:29:41 -0600

    Fix daysBetweenDatesNoWeekends, change friday for sunday on if condition and change var to let/const when is possible

--- common/utils/timeValidation.ts
+++ common/utils/timeValidation.ts
@@ -20,11 +20,12 @@ export function daysBetweenDates(startDate: Date, endDate: Date): Date[] {
   
 export function daysBetweenDatesNoWeekends(startDate: Date, endDate: Date): Date[] {
   let dates = [];
-  var newStartDate = new Date(startDate);
-  var newEndDate = new Date(endDate);
+  const newStartDate = new Date(startDate);
+  const newEndDate = new Date(endDate);
   const copyStartDate = new Date(newStartDate.getTime());
+  
   while (newEndDate >= copyStartDate) {
-    if (copyStartDate.getDay() !== 5 && copyStartDate.getDay() !== 6) {
+    if (copyStartDate.getDay() !== 0 && copyStartDate.getDay() !== 6) {
       const date = new Date(copyStartDate);
       dates.push(date);
     }
@@ -37,23 +38,25 @@ export function daysBetweenDatesNoWeekends(startDate: Date, endDate: Date): Date
   
   
 export function diffrenceBetweenDates(startDate: Date, endDate: Date) {
-  var newStartDate = new Date(startDate);
-  var newEndDate = new Date(endDate);
-  return ((newEndDate.getTime() - newStartDate.getTime()) / (1000*60*60*24))+1;
+  const newStartDate = new Date(startDate);
+  const newEndDate = new Date(endDate);
+
+  return ((newEndDate.getTime() - newStartDate.getTime()) / (1000*60*60*24)) + 1;
 }
   
 export function diffrenceBetweenDatesNoWeekends(startDate: Date, endDate: Date) {
-  var newStartDate = new Date(startDate);
-  var newEndDate = new Date(endDate);
-  return differenceInBusinessDays(newEndDate,newStartDate)+1;
+  const newStartDate = new Date(startDate);
+  const newEndDate = new Date(endDate);
+
+  return differenceInBusinessDays(newEndDate,newStartDate) + 1;
 }
 
-export function getCurrentDate(separator=''){
+export function getCurrentDate(separator = ''){
 
-  let newDate = new Date()
-  let date = newDate.getDate();
-  let month = newDate.getMonth() + 1;
-  let year = newDate.getFullYear();
+  const newDate = new Date()
+  const date = newDate.getDate();
+  const month = newDate.getMonth() + 1;
+  const year = newDate.getFullYear();
   
   return `${year}${separator}${month<10?`0${month}`:`${month}`}${separator}${date}`
 }
